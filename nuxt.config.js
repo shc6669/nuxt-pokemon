a1f9c5df79cb12d759e0b1be4192eab6352fb3bd
Chandrayana Putra <shc6669@gmail.com>
2022-04-04 17:44:33 +0800

    css and scss compile and add other needed plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,14 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-pokemon',
+    title: 'Nuxt Pokemon App',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
+      { hid: 'description', name: 'description', content: 'This is Nuxt App with fetch Pokemon API' },
+      { name: 'theme-style-mode', content: '1' }, // 0 = light, 1 = dark
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -18,7 +17,26 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/vendor/bootstrap.min.css',
+    '@/assets/css/vendor/nice-select.css',
+    '@/assets/css/plugins/feature.css',
+    '@/assets/scss/style.scss',
   ],
+
+  axios: {
+    baseURL: 'http://localhost:3000',
+    proxy: true
+  },
+
+  proxy: {
+    '/api': {
+      target: 'https://pokeapi.co/api/v2/',
+      pathRewrite: {
+        '^/api/v2/': ''
+      },
+      changeOrigin: true
+    }
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
